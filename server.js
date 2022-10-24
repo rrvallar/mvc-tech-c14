f43497ea3dafad529b2987e9e94ab8c9305de62e
rrvallar <rrvallar@gmail.com>
2022-10-23 19:02:42 -0700

    update server

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const sess = {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session(sess));
 
